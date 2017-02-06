00b974da82bbefe2dca7951ff35ac22e5626744e
Aaron Cooper <acooper@inductiveautomation.com>
2017-02-06 15:30:05 -0800

    Complete Rework
    Efficiency is over 9,000

--- dev/fbAutoEdits.user.js
+++ dev/fbAutoEdits.user.js
@@ -5,55 +5,50 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @include      http://fogbugz/
 // @include      http://10.20.1.37/
-// @version      1.0.5
+// @version      1.0.6
 // @updateURL    https://github.com/aHoyleCooper/fbScripts/raw/master/dev/fbAutoEdits.user.js
 // ==/UserScript==
 
-var oldVals = [];
-var newVals = [];
-
-var checkDropdownValues = function () {
-    var urlCheck = window.location.href;
-    if (urlCheck.indexOf("edit") > -1) {
-        oldVals = newVals;
-        newVals = [$(".droplist-text:eq(7)").val(), $(".droplist-text:eq(8)").val()];
-        if (oldVals[0] !== newVals[0] || oldVals[1] !== newVals[1] && (newVals[0] !== undefined && newVals[1] !== undefined)) {
-            // console.log("Priority updating");
-            var level = -1;
-            var priority = $(".droplist-text:eq(9)");
-            if (newVals[0] === "Critical" || newVals[0] === "High") {
-                if (newVals[1] === "Low") {
-                    // console.log("Setting priority to Yellow");
-                    level = 2;
-                } else {
-                    // console.log("Setting priority to Red");
-                    level = 1;
-                }
-            } else if (newVals[0] === "Medium") {
-                if (newVals[1] === "Low") {
-                    // console.log("Setting priority to Green");
-                    level = 3;
-                } else {
-                    // console.log("Setting priority to Yellow");
-                    level = 2;
-                }
-            } else if (newVals[0] == "--" || newVals[1] == "--") {
-                // console.log("Setting priority to Undecided")
-                level = 7;
-            } else {
-                // console.log("Setting priority to Green");
-                level = 3;
-            }
-            var levelDict = {1:"1 - Red", 2:"2 - Yellow", 3:"3 - Green", 7:"7 - Undecided"};
-            if (level > -1) {
-                // console.log("Attempting to set Priority to:", levelDict[level]);
-                priority.val(levelDict[level]);
-                $($("#ixPriority option")[level - 1]).attr('selected', 'selected');
-            }
+function setPriority(impact, probability) {
+    var level = -1;
+    if (impact == "--" || probability == "--") {
+        console.log("Setting priority to Undecided");
+        level = 7;
+    } else if (impact === "Critical" || impact === "High") {
+        if (probability === "Low") {
+            console.log("Setting priority to Yellow");
+            level = 2;
+        } else if (probability != "--") {
+            console.log("Setting priority to Red");
+            level = 1;
         }
+    } else if (impact === "Medium") {
+        if (probability === "Low") {
+            console.log("Setting priority to Green");
+            level = 3;
+        } else if (probability != "--") {
+            console.log("Setting priority to Yellow");
+            level = 2;
+        }
+    } else {
+        console.log("Setting priority to Green");
+        level = 3;
     }
-};
+    var levelDict = {1:"1 - Red", 2:"2 - Yellow", 3:"3 - Green", 7:"7 - Undecided"};
+    if (level > -1) {
+        return {"level":level, "levelString":levelDict[level]};
+    } else {
+        return {"level":7, "levelString":"7 - Undecided"};
+    }
+}
 
-$(document).ready(function() {
-    setInterval(checkDropdownValues, 1000);
+$(document.body).on('click', '#idDropList_customerximpactu43_oDropList div', function(){
+    var newPriority = setPriority($(this).text(), $(".droplist-text:eq(8)").val());
+    $(".droplist-text:eq(9)").val(newPriority.levelString);
+    $($("#ixPriority option")[newPriority.level - 1]).attr('selected', 'selected');
+});
+
+$(document.body).on('click', '#idDropList_probabilityxofxoccurrencer04_oDropList div', function(){
+    var newPriority = setPriority($(".droplist-text:eq(7)").val(), $(this).text());
+    $(".droplist-text:eq(9)").val(newPriority.levelString);
 });
