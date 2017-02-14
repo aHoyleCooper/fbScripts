36eb29d3ae3bd3404b89973917f3ef7bb509645e
Aaron Cooper <acooper@inductiveautomation.com>
2017-02-14 15:49:38 -0800

    Fixed priority selection problem, added scroll blocking on a few dropdowns

--- dev/fbAutoEdits.user.js
+++ dev/fbAutoEdits.user.js
@@ -5,7 +5,7 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @include      http://fogbugz/
 // @include      http://10.20.1.37/
-// @version      1.0.8
+// @version      1.0.9
 // @updateURL    https://github.com/aHoyleCooper/fbScripts/raw/master/dev/fbAutoEdits.user.js
 // ==/UserScript==
 
@@ -45,16 +45,35 @@ function setPriority(impact, probability) {
 $(document.body).on('click', '#idDropList_customerximpactu43_oDropList div', function(){
     var newPriority = setPriority($(this).text(), $(".droplist-text:eq(8)").val());
     $(".droplist-text:eq(9)").val(newPriority.levelString);
+    $("#ixPriority option").removeAttr('selected');
     $($("#ixPriority option")[newPriority.level - 1]).attr('selected', 'selected');
 });
 
 $(document.body).on('click', '#idDropList_probabilityxofxoccurrencer04_oDropList div', function(){
     var newPriority = setPriority($(".droplist-text:eq(7)").val(), $(this).text());
     $(".droplist-text:eq(9)").val(newPriority.levelString);
-    $(("#ixPriority option")[newPriority.level - 1]).attr('selected', 'selected');
+    $("#ixPriority option").removeAttr('selected');
+    $($("#ixPriority option")[newPriority.level - 1]).attr('selected', 'selected');
+});
+$(document.body).on('wheel', '#idDropList_ixPersonAssignedTo_oDropList', function(e){
+    var scrlAmt = e.originalEvent.deltaY;
+    if (scrlAmt < 0 && $(this).scrollTop() === 0) {
+        e.originalEvent.preventDefault();
+    } else if (scrlAmt > 0 && $(this).scrollTop() >= $(this).prop("scrollHeight") - $(this).outerHeight()) {
+        e.originalEvent.preventDefault();
+    }
 });
 
-$(document.body).on('wheel', '#idDropList_ixPersonAssignedTo_oDropList', function(e){
+$(document.body).on('wheel', '#idDropList_ixPersonAssignedToOverrideDropDown_assign0_oDropList', function(e){
+    var scrlAmt = e.originalEvent.deltaY;
+    if (scrlAmt < 0 && $(this).scrollTop() === 0) {
+        e.originalEvent.preventDefault();
+    } else if (scrlAmt > 0 && $(this).scrollTop() >= $(this).prop("scrollHeight") - $(this).outerHeight()) {
+        e.originalEvent.preventDefault();
+    }
+});
+
+$(document.body).on('wheel', '#idDropList_ixProject_oDropList', function(e){
     var scrlAmt = e.originalEvent.deltaY;
     if (scrlAmt < 0 && $(this).scrollTop() === 0) {
         e.originalEvent.preventDefault();
