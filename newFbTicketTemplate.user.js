b4cd37bf3ea2cffd6d64da90a53b8b6914b60215
Aaron Cooper <acooper@inductiveautomation.com>
2017-10-31 15:15:18 -0700

    Added fogbugz.ia.local to includes, since some are using that url format

--- newFbTicketTemplate.user.js
+++ newFbTicketTemplate.user.js
@@ -4,6 +4,7 @@
 // @match        http://*/default.asp?command=new&pg=pgEditBug
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @include      http://fogbugz/
+// @include      http://fogbugz.ia.local/
 // @include      http://10.20.1.37/
 // @version      1.0.8
 // @updateURL    https://github.com/aHoyleCooper/fbScripts/raw/master/newFbTicketTemplate.user.js
